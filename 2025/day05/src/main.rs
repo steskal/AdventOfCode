ddf395e5d494940fde4a6f7778816e09f0dd9439
steskal <steskal@gmail.com>
2025-12-09 12:01:29 -0500

    AoC 2025 Day5 Part two finished

--- 2025/day05/src/main.rs
+++ 2025/day05/src/main.rs
@@ -13,7 +13,11 @@ fn main() {
 	let mut boundary_bottom:	Vec<u64>	= Vec::new();
 	let mut boundary_top: 		Vec<u64>	= Vec::new();
 	
+	let mut boundary_tuples:    Vec<(u64, u64)>	= Vec::new();
+	let mut walk_top_fresh:		u64				= 0;
+	
 	let mut count_fresh: u64				= 0;
+	let mut fresh_ids_count: u64			= 0;
 	
 	for line_result in reader.lines() {
 		let line	= line_result.unwrap();
@@ -27,9 +31,11 @@ fn main() {
 			match line.split_once('-') {
 				Some((low, high)) => {
 					
-					boundary_bottom.push(low.parse().expect("Cannot parse lower bound"));
-					boundary_top.push(high.parse().expect("Cannot parse upper bound"));
-					
+					let bottom: u64	= low.parse().expect("Cannot parse lower bound");
+					let top: u64	=high.parse().expect("Cannot parse upper bound");
+					boundary_bottom.push(bottom);
+					boundary_top.push(top);
+					boundary_tuples.push((bottom, top));
 				},
 				None => {
 					panic!("Invalid range format");
@@ -44,8 +50,33 @@ fn main() {
 		}
 	}
 	
-	// RESULT
+	// RESULT part one
 	println!("Fresh: {}", count_fresh);
+	
+	//Part two:
+	boundary_tuples.sort_by_key(|&(key, _)| key);
+	
+	for idx in 0..boundary_tuples.len() {
+		if idx == 0 {
+			fresh_ids_count += boundary_tuples[idx].1 - boundary_tuples[idx].0 + 1;
+			walk_top_fresh	 = boundary_tuples[idx].1;
+		} else {
+			if boundary_tuples[idx].1 <= walk_top_fresh {
+				continue;
+			}
+			
+			if boundary_tuples[idx].0 <= walk_top_fresh {
+				fresh_ids_count	+= boundary_tuples[idx].1 - walk_top_fresh;
+			} else {
+				fresh_ids_count	+= boundary_tuples[idx].1 - boundary_tuples[idx].0 +1;
+			}
+			
+			walk_top_fresh = boundary_tuples[idx].1;
+		}
+	}
+	
+	// RESULT part two
+	println!("All fresh: {}", fresh_ids_count);
 }
 
 fn is_in_range(inventory_id: u64, bottoms: &Vec<u64>, tops: &Vec<u64> ) -> bool {
